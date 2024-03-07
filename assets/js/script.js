049429b862cfa51fcc43bba58cfe6e6aea6a9bdf
Joel-De <71618484+Joel-De@users.noreply.github.com>
2024-03-06 23:07:07 -0500

    add languages to projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,83 @@
-'use strict';
+"use strict";
 
+class LogoArduino extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <img
+        src="./assets/images/dev_logos/arduino-logo-wordmark.svg"
+        alt="design icon"
+        />
+      `;
+    LogoArduino;
+  }
+}
+customElements.define("logo-arduino", LogoArduino);
 
+class LogoMLIR extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <img
+          src="./assets/images/dev_logos/mlir-original-wordmark.svg"
+          alt="design icon"
+          />
+        `;
+    LogoMLIR;
+  }
+}
+customElements.define("logo-mlir", LogoMLIR);
+
+class LogoJava extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <img
+          src="./assets/images/dev_logos/java-original-wordmark.svg"
+          alt="design icon"
+          />
+        `;
+    LogoJava;
+  }
+}
+customElements.define("logo-java", LogoJava);
+
+class LogoSystemVerilog extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <img
+          src="./assets/images/dev_logos/systemverilog-original.svg"
+          alt="design icon"
+          />
+        `;
+    LogoSystemVerilog;
+  }
+}
+customElements.define("logo-system-verilog", LogoSystemVerilog);
+
+class LogoJavaScript extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+          <img
+            src="./assets/images/dev_logos/javascript-original.svg"
+            alt="design icon"
+            />
+          `;
+    LogoJavaScript;
+  }
+}
+customElements.define("logo-javascript", LogoJavaScript);
 
 // element toggle function
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
-
-
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
 
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
@@ -31,29 +94,29 @@ const modalDate = document.querySelector("[data-modal-date]");
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
-}
+};
 
 // add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
   testimonialsItem[i].addEventListener("click", function () {
-
     // modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-    modalDate.innerHTML = this.querySelector("[data-testimonials-date]").innerHTML;
-    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    modalDate.innerHTML = this.querySelector(
+      "[data-testimonials-date]"
+    ).innerHTML;
+    modalTitle.innerHTML = this.querySelector(
+      "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      "[data-testimonials-text]"
+    ).innerHTML;
 
     testimonialsModalFunc();
-
   });
-
 }
 
 // add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
-
-
 
 // custom select variables
 const select = document.querySelector("[data-select]");
@@ -61,17 +124,17 @@ const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+select.addEventListener("click", function () {
+  elementToggleFunc(this);
+});
 
 // add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
-
   });
 }
 
@@ -79,9 +142,7 @@ for (let i = 0; i < selectItems.length; i++) {
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -89,18 +150,14 @@ const filterFunc = function (selectedValue) {
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
-}
+};
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
   filterBtn[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
@@ -108,12 +165,8 @@ for (let i = 0; i < filterBtn.length; i++) {
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
-
   });
-
 }
-
-
 
 // contact form variables
 const form = document.querySelector("[data-form]");
@@ -123,18 +176,14 @@ const formBtn = document.querySelector("[data-form-btn]");
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
-
     // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
     }
-
   });
 }
-
-
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
@@ -143,7 +192,6 @@ const pages = document.querySelectorAll("[data-page]");
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
-
     for (let i = 0; i < pages.length; i++) {
       if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
         pages[i].classList.add("active");
@@ -154,6 +202,5 @@ for (let i = 0; i < navigationLinks.length; i++) {
         navigationLinks[i].classList.remove("active");
       }
     }
-
   });
 }
